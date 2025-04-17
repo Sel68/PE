6a40a4b115b9ea4af874430a0fa6768b24d937c3
sel68 <saadhasanellahie@gmail.com>
2025-04-17 21:37:51 +0500

    corrected

--- sieve.py
+++ sieve.py
@@ -2,10 +2,10 @@ import numpy as np
 
 def sieve(hi=int(1e6)):
     if hi < 2:
-        return np.zeros(hi, dtype=bool)
-    is_prime = np.ones(hi, dtype=bool)
-    is_prime[0] = False
-    for p in range(3, int(hi**0.5) + 1, 2):
+        return np.zeros(hi, dtype = bool)
+    is_prime = np.ones(hi, dtype=bool)   
+    is_prime[0] = is_prime[1] = 0          
+    for p in range(2, int(hi**0.5) + 1):
         if is_prime[p]:
-            is_prime[p*p::2*p] = False
+            is_prime[p*p::p] = False
     return is_prime
